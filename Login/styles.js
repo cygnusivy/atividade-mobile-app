c3a93490079c17d7ee914063a34c57eddda42c9f
Everton Nascimento <nascimentoeverton568@gmail.com>
2023-06-01 22:21:26 -0300

    commit inicial

---
+++ Login/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+const Container = styled.View`
+flex: 1;
+background-color: rgb(243, 252, 231);
+align-items: center;
+justify-content: center;
+padding: 20px;
+`;
+
+export default Container;
---
+++ styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native';
+
+export const Input = styled.TextInput`
+width: 100%,
+height: 30px,
+background-color: rgba(212, 221, 208, 0.698),
+border: none,
+border-radius: 20px,
+padding-left: 4.5%,
+margin-bottom: 10px,
+color: black,
+`;
+
+export const ButtonLogin = styled.TouchableOpacity`
+width: 100%,
+height: 30px,
+cursor: pointer,
+color: rgb(152, 192, 101),
+background-color: rgb(22, 27, 49),
+border-radius: 20px,
+justify-content: center,
+align-items: center,
+`;
